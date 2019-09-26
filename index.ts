ed75cc03fa7af7aa3cea7e8502e139b6230bf5da
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2019-09-26 21:03:57 +0100

    interfaces

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("hello world!");
+import { base64encode } from './base64encoders'
+
+let input = 'Hello ts'
+let output = 'SGVsbG8gV29ybGQ'
+console.log("INPUT: ", input);
+console.log("OUTPUT: ", base64encode(input));
+console.log("EXPECT:", output);
